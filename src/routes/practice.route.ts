82f08e838830808d0e256b4fe69637984361131e
A-N-M-Rashed <a.n.m.rashed045@gmail.com>
2026-08-27 00:11:43 +0600

    Task 2: Find Queries

--- src/routes/practice.route.ts
+++ src/routes/practice.route.ts
@@ -8,3 +8,45 @@ export const practiceRouter = Router();
 practiceRouter.get("/", (req, res) => {
   res.json({ message: "Practice route working!" });
 });
+
+//Task 2: Show All
+practiceRouter.get(
+    "/all",
+    async(req, res)=>{
+        const db= getDatabase();
+
+        const result =await db.collection("student").find({}).toArray();
+
+        res.json(result);
+    },
+);
+
+//Task 2: Show One
+practiceRouter.get(
+    "/one-student",
+    async(req, res)=>{
+        const db= getDatabase();
+
+        const result =await db.collection("student").findOne({ name: "Rafiq" });
+
+        res.json(result);
+    },
+);
+
+//Task 2: Show Projection
+practiceRouter.get(
+    "/projection",
+    async(req, res)=>{
+        const db= getDatabase();
+
+        const result =await db.collection("student").aggregate([{
+            $project:{
+                _id: 0,
+                name: 1,
+                department: 1
+            }
+        }]).toArray();
+
+        res.json(result);
+    },
+);
