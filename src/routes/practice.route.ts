67a9add70396d749a796747e955ff2f3c0a6b97b
A-N-M-Rashed <a.n.m.rashed045@gmail.com>
2026-08-27 00:31:00 +0600

    Task 3: Filter Queries

--- src/routes/practice.route.ts
+++ src/routes/practice.route.ts
@@ -39,13 +39,79 @@ practiceRouter.get(
     async(req, res)=>{
         const db= getDatabase();
 
-        const result =await db.collection("student").aggregate([{
-            $project:{
+        const result =await db.collection("student").find({}, {
+            projection:{
                 _id: 0,
                 name: 1,
                 department: 1
             }
-        }]).toArray();
+        }).toArray();
+
+        res.json(result);
+    },
+);
+
+//Task 3: Age greater than 20
+practiceRouter.get(
+    "/age-above-20",
+    async(req, res)=>{
+        const db= getDatabase();
+
+        const result =await db.collection("student").aggregate([
+            {
+                $match: {
+                    age: {$gt: 20},
+                },
+            },
+        ]).toArray();
+
+        res.json(result);
+    },
+);
+
+//Task 3: CSE Department
+practiceRouter.get(
+    "/cse",
+    async(req, res)=>{
+        const db= getDatabase();
+
+        const result =await db.collection("student").aggregate([
+            {
+                $match: {
+                    department: {
+                        $regex: "cse",
+                        $options: "i",
+                    },
+                },
+            },
+        ]).toArray();
+
+        res.json(result);
+    },
+);
+
+//Task 3: greater than or equal to 3.50
+practiceRouter.get(
+    "/above-3.5",
+    async(req, res)=>{
+        const db= getDatabase();
+
+        const result =await db.collection("student").aggregate([
+            {
+                $match: {
+                    cgpa: {
+                        $gte: 3.50
+                    },
+                },
+            },{
+                $project: {
+                    _id: 0,
+                    name: 1,
+                    department: 1,
+                    cgpa: 1
+                }
+            }
+        ]).toArray();
 
         res.json(result);
     },
