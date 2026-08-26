230c59ce9fc678057c9994c58c6646e1763cd983
A-N-M-Rashed <a.n.m.rashed045@gmail.com>
2026-08-27 00:41:30 +0600

    Task 4: Sort & Limit

--- src/routes/practice.route.ts
+++ src/routes/practice.route.ts
@@ -6,16 +6,16 @@ import { getDatabase } from "../db";
 export const practiceRouter = Router();
 
 practiceRouter.get("/", (req, res) => {
-  res.json({ message: "Practice route working!" });
+    res.json({ message: "Practice route working!" });
 });
 
 //Task 2: Show All
 practiceRouter.get(
     "/all",
-    async(req, res)=>{
-        const db= getDatabase();
+    async (req, res) => {
+        const db = getDatabase();
 
-        const result =await db.collection("student").find({}).toArray();
+        const result = await db.collection("student").find({}).toArray();
 
         res.json(result);
     },
@@ -24,10 +24,10 @@ practiceRouter.get(
 //Task 2: Show One
 practiceRouter.get(
     "/one-student",
-    async(req, res)=>{
-        const db= getDatabase();
+    async (req, res) => {
+        const db = getDatabase();
 
-        const result =await db.collection("student").findOne({ name: "Rafiq" });
+        const result = await db.collection("student").findOne({ name: "Rafiq" });
 
         res.json(result);
     },
@@ -36,11 +36,11 @@ practiceRouter.get(
 //Task 2: Show Projection
 practiceRouter.get(
     "/projection",
-    async(req, res)=>{
-        const db= getDatabase();
+    async (req, res) => {
+        const db = getDatabase();
 
-        const result =await db.collection("student").find({}, {
-            projection:{
+        const result = await db.collection("student").find({}, {
+            projection: {
                 _id: 0,
                 name: 1,
                 department: 1
@@ -54,13 +54,13 @@ practiceRouter.get(
 //Task 3: Age greater than 20
 practiceRouter.get(
     "/age-above-20",
-    async(req, res)=>{
-        const db= getDatabase();
+    async (req, res) => {
+        const db = getDatabase();
 
-        const result =await db.collection("student").aggregate([
+        const result = await db.collection("student").aggregate([
             {
                 $match: {
-                    age: {$gt: 20},
+                    age: { $gt: 20 },
                 },
             },
         ]).toArray();
@@ -72,10 +72,10 @@ practiceRouter.get(
 //Task 3: CSE Department
 practiceRouter.get(
     "/cse",
-    async(req, res)=>{
-        const db= getDatabase();
+    async (req, res) => {
+        const db = getDatabase();
 
-        const result =await db.collection("student").aggregate([
+        const result = await db.collection("student").aggregate([
             {
                 $match: {
                     department: {
@@ -93,17 +93,17 @@ practiceRouter.get(
 //Task 3: greater than or equal to 3.50
 practiceRouter.get(
     "/above-3.5",
-    async(req, res)=>{
-        const db= getDatabase();
+    async (req, res) => {
+        const db = getDatabase();
 
-        const result =await db.collection("student").aggregate([
+        const result = await db.collection("student").aggregate([
             {
                 $match: {
                     cgpa: {
                         $gte: 3.50
                     },
                 },
-            },{
+            }, {
                 $project: {
                     _id: 0,
                     name: 1,
@@ -116,3 +116,58 @@ practiceRouter.get(
         res.json(result);
     },
 );
+
+//Task 4: Sort by CGPA (Highest to Lowest)
+practiceRouter.get("/sort", async (req, res) => {
+    const db = getDatabase();
+
+    const result = await db
+        .collection("student")
+        .aggregate([
+            {
+                $sort: {
+                    cgpa: -1,
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    name: 1,
+                    cgpa: 1,
+                    department: 1,
+                },
+            },
+        ])
+        .toArray();
+
+    res.json(result);
+});
+
+//Task 4: limit first three students
+practiceRouter.get("/limit", async (req, res) => {
+    const db = getDatabase();
+
+    const result = await db
+        .collection("student")
+        .aggregate([
+            {
+                $sort: {
+                    cgpa: -1,
+                },
+            }, 
+            {
+                $limit: 3
+            },
+            {
+                $project: {
+                    _id: 0,
+                    name: 1,
+                    cgpa: 1,
+                    department: 1,
+                },
+            },
+        ])
+        .toArray();
+
+    res.json(result);
+});
